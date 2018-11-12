a7c3cfbea11605d7170dd87f1719a351af8a387a
Leolainez <meraelizabeth_93@hotmail.com>
2018-11-12 00:39:00 -0600

    Proveedores

---
+++ Rentas/BL.Rentas/ProveedoresBL.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+    public class ProveedoresBL
+    {
+        Contexto _contexto;
+
+        public BindingList<Proveedores> ListaProveedores { get; set; }
+
+        public ProveedoresBL()
+        {
+            _contexto = new Contexto();
+            ListaProveedores = new BindingList<Proveedores>();
+
+
+        }
+
+        public BindingList<Proveedores> ObtenerProverdores()
+        {
+            _contexto.Provedores.Load();
+            ListaProveedores = _contexto.Provedores.Local.ToBindingList();
+
+            return ListaProveedores;
+        }
+
+        public void CancelarCambios()
+        {
+            foreach (var item in _contexto.ChangeTracker.Entries())
+            {
+                item.State = EntityState.Unchanged;
+                item.Reload();
+            }
+        }
+
+        public Resultado GuardarProveedores(Proveedores proveedores)
+        {
+            var resultado = Validar(proveedores);
+            if (resultado.Exitoso == false)
+            {
+                return resultado;
+            }
+
+            _contexto.SaveChanges();
+
+            resultado.Exitoso = true;
+            return resultado;
+        }
+
+
+        public void AgregarProveedores()
+        {
+            var nuevoProveedor = new Proveedores();
+            _contexto.Provedores.Add(nuevoProveedor);
+        }
+
+        public bool EliminarProveedores(int id)
+        {
+            foreach (var proveedores in ListaProveedores.ToList())
+            {
+                if (proveedores.Id == id)
+                {
+                    ListaProveedores.Remove(proveedores);
+                    _contexto.SaveChanges();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private Resultado Validar(Proveedores proveedores)
+        {
+            var resultado = new Resultado();
+            resultado.Exitoso = true;
+
+            if (proveedores == null)
+            {
+                resultado.Mensaje = "Agregue un proveedor valido";
+                resultado.Exitoso = false;
+
+                return resultado;
+            }
+
+            if (string.IsNullOrEmpty(proveedores.Nombre) == true)
+            {
+                resultado.Mensaje = "Ingrese un Nombre";
+                resultado.Exitoso = false;
+            }
+
+          
+
+
+            return resultado;
+        }
+
+    }
+
+   
+
+
+   
+
+   
+
+    public class Proveedores
+    {
+        
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Direccion { get; set; }
+            public int Telefono { get; set; }
+            public string CorreoElectronico { get; set; }
+
+        
+           
+        public bool Activo { get; set; }
+
+            public Proveedores()
+        {
+            Activo = true;
+        }
+
+    }
+}
---
+++ Rentas/Win.Rentas/FormProveedores.cs
@@ -0,0 +1,112 @@
+﻿using BL.Rentas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormProveedores : Form
+    {
+        ProveedoresBL _proveedoresBL;
+
+
+        public FormProveedores()
+        {
+            InitializeComponent();
+            _proveedoresBL = new ProveedoresBL();
+
+            // ListaProveedoresBindingSource.DataSource = _proveedoresBL.ObtenerProverdores();
+            listaProveedoresBindingSource.DataSource = _proveedoresBL.ObtenerProverdores();
+
+        }
+
+        
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            if (idTextBox.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox.Text);
+                    Eliminar(id);
+                }
+
+
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            var resultado = _proveedoresBL.EliminarProveedores(id);
+
+            if (resultado == true)
+            {
+                listaProveedoresBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el Proveedor");
+            }
+        }
+
+        private void listaProveedoresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaProveedoresBindingSource.EndEdit();
+            var proveedores = (Proveedores)listaProveedoresBindingSource.Current;
+
+            var resultado = _proveedoresBL.GuardarProveedores(proveedores);
+
+            if (resultado.Exitoso == true)
+            {
+                listaProveedoresBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Proveedor guardado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            Cancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _proveedoresBL.AgregarProveedores();
+            listaProveedoresBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void Cancelar_Click(object sender, EventArgs e)
+        {
+
+            _proveedoresBL.CancelarCambios();
+            DeshabilitarHabilitarBotones(true);
+        }
+
+        private void activoLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
